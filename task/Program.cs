7414434a17baaa82065255a8d78972a13d59e4f4
Anna Bolotova <anechkamilaha69@gmail.com>
2023-03-21 22:02:49 +0300

    was written code

---
+++ task/Program.cs
@@ -0,0 +1,35 @@
+﻿int NewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<3)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+    return count;
+}
+void FillNewArray(string[] array, string[] newarray)
+{
+    int g = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<3)
+        {
+            newarray[g] = array[i];
+            g++;
+        }
+    }
+}
+void Print(string[] array)
+{
+    System.Console.WriteLine("["+ string.Join(",", array)+"]");
+}
+
+string[] ourarray = {"more", "-5", "dog", "beautiful","4"};
+int n = NewArray(ourarray);
+string[] ourarray2 = new string[n];
+FillNewArray(ourarray,ourarray2);
+Print(ourarray2);
